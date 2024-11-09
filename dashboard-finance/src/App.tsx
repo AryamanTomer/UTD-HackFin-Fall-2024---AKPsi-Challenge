f3a06388f18c7c4ac751b790f97708f8dac0e8cc
AryamanTomer <101023524+AryamanTomer@users.noreply.github.com>
2024-11-09 15:59:01 -0600

    More libraries

--- dashboard-finance/src/App.tsx
+++ dashboard-finance/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Box width="100%" height="100%" padding="1rem 2rem 4rem 3rem">
-
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem">
+          
         </Box>
       </BrowserRouter>
     </div>

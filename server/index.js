e2c9d211b1548af8546b471853e082143a87e4b5
Temiloluwa <97484828+Temii-mp3@users.noreply.github.com>
2024-11-09 13:20:43 -0600

    Integrated gemini api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import {GoogleGenerativeAI} from "@google/generative-ai";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -20,6 +21,27 @@ const PORT = process.env.PORT || 9000;
 mongoose
     .connect(process.env.MONGO_URL)
     .then(async () => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        app.get('/budget_drafter', async (req, res) => {
+            try{
+                //call fetchBudgetData and store it in data
+                const data = await fetchBudgetData();
+                //send date
+                res.send(data.response.candidates[0].content.parts[0].text);
+            }catch(error){
+                res.status(500).send({error: "Failed to fetch data"}); //handle err
+            }
+        })
     })
     .catch((error) => console.log(`${error} did not connect`));
+
+    async function fetchBudgetData(){
+        //new instance of gemini w api key
+        const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const prompt = "This is just a test, say hi";
+    
+    const result = await model.generateContent(prompt); 
+    return result;  
+    }

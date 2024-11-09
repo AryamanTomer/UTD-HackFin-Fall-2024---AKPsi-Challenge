b611db9d229b9f6a2181d55dc51a8ae96628700b
AryamanTomer <101023524+AryamanTomer@users.noreply.github.com>
2024-11-09 11:44:29 -0600

    Starting Application
    This is a starting application

---
+++ dashboard-finance/src/App.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 3rem">
+
+        </Box>
+      </BrowserRouter>
+    </div>
+  )
+}
+export default App
